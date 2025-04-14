f88c47d155e9b40eec9357e923dfb78629a48def
Pedro Fonseca <contatopedrorn@gmail.com>
2025-04-14 19:00:35 -0300

    Refactor UserAdmin to enhance fieldsets and maintain consistent ordering

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,18 +1,40 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 from accounts.models import User
 
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    list_display = (
-        "email",
-        "document",
-        "is_staff",
-        "is_active",
-    )
+    ordering = ["email"]
+    list_display = ("email", "document", "is_staff", "is_active")
     search_fields = ("email", "document")
     list_filter = ("is_staff", "is_superuser", "is_active")
-    fieldsets = BaseUserAdmin.fieldsets + (
-        ("Informações adicionais", {"fields": ("document",)}),
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Informações pessoais"), {"fields": ("document",)}),
+        (
+            _("Permissões"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Datas importantes"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "document", "password1", "password2"),
+            },
+        ),
     )

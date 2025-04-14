c71e055e5aff65050e87e1046d00f1161a05b2e6
Pedro Fonseca <contatopedrorn@gmail.com>
2025-04-14 17:16:07 -0300

    Refactor URL configuration to include API routes for loans and payments

--- core/urls.py
+++ core/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+
+from django.urls import path, include
+
+api_urlpatterns = [
+    path("loans/", include("loans.urls")),
+    path("payments/", include("payments.urls")),
+]
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("api/", include(api_urlpatterns)),
 ]

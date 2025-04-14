875e2619cbcd01acce3299549c78e77400aac039
Pedro Fonseca <contatopedrorn@gmail.com>
2025-04-14 17:50:33 -0300

    Update User model and initial migration to enforce unique email and document fields

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
     document = models.CharField(
         max_length=20,
         unique=True,
@@ -10,5 +11,8 @@ class User(AbstractUser):
         help_text="Número do CPF ou CNPJ",
     )
 
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
     def __str__(self):
-        return f"{self.username} ({self.document})"
+        return f"{self.email} ({self.document})"

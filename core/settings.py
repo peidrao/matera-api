993faf61f26000c798c1fdf4a3ce08bee92f0401
Pedro Fonseca <contatopedrorn@gmail.com>
2025-04-15 13:26:20 -0300

    from dj_database_url import config add

--- core/settings.py
+++ core/settings.py
@@ -1,3 +1,6 @@
+import os
+
+from dj_database_url import config
 from datetime import timedelta
 from pathlib import Path
 
@@ -114,11 +117,12 @@ SPECTACULAR_SETTINGS = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": config(
+        default=os.getenv("DATABASE_URL", "sqlite:///db.sqlite3"),
+        conn_max_age=600,
+    )
 }
 
 

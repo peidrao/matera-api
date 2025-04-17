906333a798c2d039bc8d6061bfb0368c89fb140d
Pedro Fonseca <contatopedrorn@gmail.com>
2025-04-17 12:19:43 -0300

    add more testes

--- accounts/tests/test_views_register_account.py
+++ accounts/tests/test_views_register_account.py
@@ -2,6 +2,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from accounts.models import User
+from accounts.serializers import RegisterSerializer
 
 
 class RegisterViewTestCase(APITestCase):
@@ -18,6 +19,22 @@ class RegisterViewTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(User.objects.count(), 1)
         self.assertEqual(User.objects.first().email, self.valid_payload["email"])
+
+    def test_document_must_be_only_digits(self):
+        data = {
+            "email": "user@example.com",
+            "password": "12345678",
+            "document": "abc123456",
+        }
+
+        serializer = RegisterSerializer(data=data)
+        is_valid = serializer.is_valid()
+
+        self.assertFalse(is_valid)
+        self.assertIn("document", serializer.errors)
+        self.assertIn(
+            "O CPF/CNPJ deve conter apenas números.", serializer.errors["document"]
+        )
 
     def test_register_missing_fields(self):
         response = self.client.post(self.url, {}, format="json")

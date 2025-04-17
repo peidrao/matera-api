e64754359bd40105f50f17bf5b340f0feb9e5117
Pedro Fonseca <contatopedrorn@gmail.com>
2025-04-17 14:45:00 -0300

    fix url

--- core/urls.py
+++ core/urls.py
@@ -17,12 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularRedocView,
-    SpectacularSwaggerView,
-)
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
+                                   SpectacularSwaggerView)
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 api_urlpatterns = [
     path("", include("loans.urls")),

98f6a5c4692cb3267c92cf843eb9621aee74fa2d
Pedro Fonseca <contatopedrorn@gmail.com>
2025-04-17 13:52:50 -0300

    add pagination

--- core/settings.py
+++ core/settings.py
@@ -101,9 +101,7 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.CursorPagination",
-    "PAGE_SIZE": 10,
-    "DEFAULT_ORDERING": "-created_at",
+    "DEFAULT_PAGINATION_CLASS": "core.pagination.CreatedAtCursorPagination",
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.SearchFilter",
